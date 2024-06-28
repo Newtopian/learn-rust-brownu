a0fac64ee05e3fa8bf6c29a1e26ddcc6bdb11d95
newtopian <daigno at alphabet's>
2024-06-28 15:24:36 -0400

    finished chanpter 3

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -4,6 +4,43 @@ fn main() {
     section_3_2();
 
     another_function(3);
+
+    section_3_5();
+
+}
+
+fn section_3_5(){
+    println!("------------------ Section 3.5 ");
+
+    let number = 7;
+
+    if number < 5 {
+        println!("Condition True");
+    } else {
+        println!("Condition False");
+    }
+
+    // if is itself an expression...
+
+    let condition = true;
+
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is {number}");
+
+    // expression are really cool...  I re-gain that awesome
+    // vibe I loved so much with groovy closures !
+
+    // carefull though, expression must be type consistant
+
+    let condition = true;
+    // this line will fail because types are inconsistant 
+    // even if the code does not resolve to the wrong type
+    //let number = if condition { 5 } else { "six" };
+
+    println!("The value is {number}");
+
+    println!("<< Section 3.5 <<<<<")
 }
 
 fn another_function(x: i32){
@@ -28,7 +65,7 @@ fn section_3_2(){
 
     let tup: (i32, f64, u8) = (500, 6.4, 1);
 
-    let (x,y,_z) = tup;
+    let (_x,y,_z) = tup;
 
     //println!("the tup is : {tup}");
     // Agove will fail as default formatter does not know how to print tuples
@@ -44,10 +81,10 @@ fn section_3_2(){
     //println!("Indexed access from string {tup.0}, {tup.1}, {tup.2} -- will fail compilation !")
 
     // arrays
-    let a = [1, 2, 3, 4, 5];
+    let _a = [1, 2, 3, 4, 5];
     // arrays live on the stack and thus are fixed in size once created
     // array type must include type and lenght when declared
-    let a: [i32; 5] = [1, 2, 3, 4, 5]; 
+    let _a: [i32; 5] = [1, 2, 3, 4, 5]; 
 
     // array can be declared as such if they are to contain the same value
     let a = [3; 5]; // creates an array of lenght 5 with all 3
