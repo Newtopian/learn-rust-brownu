5fb1ea45bdddc396b5ef419edb73038548c66b1f
newtopian <daigno at alphabet's>
2024-06-28 13:06:22 -0400

    shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,11 +1,17 @@
 const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3; 
 
 fn main() {
-    let mut x = 5;
+    let x = 5;
     println!("The value of x is: {x}");
 
-    x = 6;
+    let x = x + 1;
     println!("The value of x is: {x}");
 
+    {
+        let x = x * 2;
+        println!("The value of x is now: {x}");
+    }
+
+    println!("The value of x is: {x}");
     println!("Constant is: {THREE_HOURS_IN_SECONDS}");
 }
