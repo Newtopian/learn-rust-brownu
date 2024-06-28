b94d1ee6a5187240ff63e03d4a4bd30ae87e15be
newtopian <daigno at alphabet's>
2024-06-28 12:55:52 -0400

    Constants

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,11 @@
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3; 
+
 fn main() {
     let mut x = 5;
     println!("The value of x is: {x}");
 
     x = 6;
-    println!("The value of x is: {x}")
+    println!("The value of x is: {x}");
+
+    println!("Constant is: {THREE_HOURS_IN_SECONDS}");
 }
